66960f9767cd66af5f41c1d5972a21e0a25b3fe2
Gorbataras <gorbataras@gmail.com>
2020-02-05 13:31:29 -0500

    added pet obj. added classes/ to autoload

---
+++ classes/pet.php
@@ -0,0 +1,18 @@
+<?php
+
+
+class pet
+{
+	private $_name;
+	private $_color;
+
+	function eat() {
+		echo "Pet is eating<br>";
+	}
+
+	function talk() {
+		echo "Pet is talking<br>";
+	}
+
+
+}
--- index.php
+++ index.php
@@ -3,12 +3,26 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
 require_once('vendor/autoload.php');
+require('classes/pet.php');
+
+session_start();
 
 $f3 = Base::instance();
 
 $f3->route('GET /', function () {
-$view = new Template();
-echo $view->render('views/my-pets.php');
+
+	$pet1 = new Pet();
+	$pet1 ->eat();
+	$pet1 ->talk();
+
+
+
+
+
+
+
+	//$view = new Template();
+	//echo $view->render('views/my-pets.php');
 });
 
 //Run
